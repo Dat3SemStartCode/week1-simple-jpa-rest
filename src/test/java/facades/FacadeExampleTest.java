3bd31b7a0b77c96f262cb26008566dbb60350d63
Thomas-Hartmann <thomas@webtrade.dk>
2019-08-26 14:56:07 +0200

    added support for junit 5 with test dependencies

--- src/test/java/facades/FacadeExampleTest.java
+++ src/test/java/facades/FacadeExampleTest.java
@@ -7,39 +7,45 @@ package facades;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+//import org.junit.After;
+//import org.junit.AfterClass;
+//import org.junit.Before;
+//import org.junit.BeforeClass;
+////import org.junit.jupiter.api.Test;
+//import static org.junit.Assert.*;
 
 /**
  *
  * @author thomas
  */
 public class FacadeExampleTest {
-    private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("putest");
-    private static FacadeExample fe = FacadeExample.getFacadeExample(emf);
+    private static final EntityManagerFactory ENF = Persistence.createEntityManagerFactory("pu");
+    private static final FacadeExample FE = FacadeExample.getFacadeExample(ENF);
     public FacadeExampleTest() {
     }
     
-    @BeforeClass
+    @BeforeAll
     public static void setUpClass() {
 //        Add code to setup entities for test before running any test methods
     }
     
-    @AfterClass
+    @AfterAll
     public static void tearDownClass() {
 //        Clean up database after test is done or use a persistence unit with drop-and-create to start up clean on every test
     }
     
-    @Before
+    @BeforeEach
     public void setUp() {
 //        Put the test database in a proper state before each test is run
     }
     
-    @After
+    @AfterEach
     public void tearDown() {
 //        Remove any data after each test was run
     }
@@ -50,6 +56,7 @@ public class FacadeExampleTest {
     @Test
     public void testSomeMethod() {
         fail("The test case is a prototype.");
+//        assertTrue(true);
     }
     
 }
