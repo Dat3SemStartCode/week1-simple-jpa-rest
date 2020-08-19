e32a352f5ef4edf4a0a4cfc5b8ada9f544e44416
Lars Mortensen (LAM - Adjunkt - Cphbusiness) <lam@cphbusiness.dk>
2020-08-19 11:52:57 +0200

    added missing import

--- src/main/java/rest/service/RenameMeResource.java
+++ src/main/java/rest/service/RenameMeResource.java
@@ -5,6 +5,7 @@ import facades.FacadeExample;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import javax.persistence.PersistenceContext;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
